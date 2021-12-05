0c98e427d842cb6458c45570a8ce2246e8829c81
ChuyingHe <heidihcy@outlook.com>
2021-12-05 18:07:42 +0100

    add routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProductList} from './products/productlist.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductList }
 ];
 
 @NgModule({
---
+++ src/app/products/productlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+    selector: 'app-home',
+    templateUrl: './productlist.component.html',
+})
+
+export class ProductList implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+  
