599225f870e35a4ec668cc034939d389399f9032
ChuyingHe <heidihcy@outlook.com>
2021-12-07 21:48:47 +0100

    start to write api request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductList } from './products/productlist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductList
   ],
   imports: [
     SharedModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/productlist.component.ts
+++ src/app/products/productlist.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../service/product.service';
+import { Observable } from 'rxjs';
+
+
 @Component({
     selector: 'app-home',
     templateUrl: './productlist.component.html',
 })
 
 export class ProductList implements OnInit {
+  
+  products!: Observable<[]>;
 
-  constructor() { }
+  constructor( private readonly productsService: ProductsService) { }
 
   ngOnInit(): void {
+    this.products = this.productsService.getProductsValue();
+
+    console.log("this.products", this.products.subscribe(async (value) => {
+      return value;
+    }));
+
   }
 
 }
