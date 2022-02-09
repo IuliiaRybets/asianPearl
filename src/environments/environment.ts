02a1fcacb72ae0a02cd8cabd76252c708ebe568e
Rybets <RYBETI@de.softlab.net>
2022-02-10 00:48:20 +0100

    bugfix/carousel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-  // ,api_base: "http://0.0.0.0:4000"
+  production: false,
+  api_base: '/api'
 };
 
 /*
