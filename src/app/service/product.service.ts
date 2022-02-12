59bc922afad2a924a16eb0fe0a2fc8c7e8915595
Chuying He <heidihcy@outlook.com>
2022-02-12 20:17:48 +0100

    added observable

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Product } from '../products/product.model';
 
 @Injectable()
@@ -8,6 +9,7 @@ export class ProductService {
     constructor(private httpClient: HttpClient, ) {}
     
     products!: Product[];
+    subject = new Subject<Product>();
 
     getProducts() {
         
@@ -15,11 +17,14 @@ export class ProductService {
         this.httpClient.get<any>('http://localhost:4000/products').subscribe(
             response => {
                 this.products = response;
+
                 console.log("response=", response, this.products);
+                this.subject.next(response);
             }
 
         );
-        console.log("products=", this.products);
+        console.log("products=", this.subject.asObservable(), this.subject);
+        return this.products;
     }
 
     productSelected = new EventEmitter<Product>();
