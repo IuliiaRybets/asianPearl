631fc7495bb2b3be97aec5c370e3537e2b0be03c
Rybets <RYBETI@de.softlab.net>
2022-02-25 17:44:09 +0100

    feature add

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [
+        NgbModule
     ],
     declarations: [
         HeaderComponent
